f56e7415c0be86f797d55b907c02bfa7af3d8b10
mweinbach <maxweinbach5@gmail.com>
2025-02-11 16:09:45 -0500

    fixed page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { PyodidePreloader } from "@/components/pyodide-preloader";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- types/html.d.ts
+++ types/html.d.ts
@@ -1,6 +1,10 @@
-declare module 'react' {
-  interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+import React from "react";
+
+declare module "react" {
+  interface InputHTMLAttributes<T> extends React.HTMLAttributes<T> {
     webkitdirectory?: string | boolean;
     directory?: string | boolean;
   }
 } 
+
+export {};
